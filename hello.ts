6754467e9b1d7b46746b4dbdda34fcab2abf8aa3
kondo takeshi <chaspy@users.noreply.github.com>
2023-11-12 17:04:17 +0900

    update

--- hello.ts
+++ hello.ts
@@ -1,18 +1,70 @@
+// ref: https://dev.classmethod.jp/articles/function_calling_nodejs/
 import OpenAI from "npm:openai";
-
-console.log("hello");
 
 const openai = new OpenAI({
   apiKey: Deno.env.get("OPENAI_API_KEY"),
 });
 
+const prompt: ChatCompletionRequestMessage = {
+  role: "user",
+  content:
+    "くらにゃんの住んでいる国の有名なイベントについて日本語で教えてください",
+};
+
+const getLivingCountry = (userName: string) => {
+  return userName === "くらにゃん" ? "日本" : "アメリカ";
+};
+
+const functions = {
+  getLivingCountry,
+} as const;
+
 async function main() {
-  const chatCompletion = await openai.chat.completions.create({
-    messages: [{ role: "user", content: "元気？" }],
+  const res = await openai.chat.completions.create({
+    messages: [prompt],
     model: "gpt-3.5-turbo",
+    function_call: "auto",
+    functions: [
+      {
+        name: "getLivingCountry",
+        description: "住んでいる国を取得",
+        parameters: {
+          type: "object",
+          properties: {
+            name: {
+              type: "string",
+              description: "ユーザー名",
+            },
+          },
+          required: ["name"],
+        },
+      },
+    ],
   });
 
-  console.log(chatCompletion);
+  const message = res.choices[0].message;
+  console.log("message:", message);
+  const functionCall = message?.function_call;
+
+  if (functionCall) {
+    const args = JSON.parse(functionCall.arguments || "{}");
+
+    const functionRes = functions[functionCall.name!](args.name);
+
+    const res2 = await openai.chat.completions.create({
+      model: "gpt-3.5-turbo",
+      messages: [
+        prompt,
+        message,
+        {
+          role: "function",
+          content: functionRes,
+          name: functionCall.name,
+        },
+      ],
+    });
+    console.log("answer", res2.data.choices[0].message);
+  }
 }
 
 main();
