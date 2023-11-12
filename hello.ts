7dbf3614cc94d0a263e7dc0a1dd961ab9de30c92
kondo takeshi <chaspy@users.noreply.github.com>
2023-11-12 11:31:05 +0900

    fix

--- hello.ts
+++ hello.ts
@@ -4,11 +4,11 @@ console.log("hello");
 
 const openai = new OpenAI({}); // Load API Key from env by default
 
-async function main(){
-    const chatComplete = await openai.completions.create({
-        messages: [(role: 'user', content: '元気？')],
-        model: 'gpt-3.5-turbo',
-    }(
+async function main() {
+  const chatCompletion = await openai.chat.completions.create({
+    messages: [{ role: "user", content: "Say this is a test" }],
+    model: "gpt-3.5-turbo",
+  });
 }
 
-main()
+main();
